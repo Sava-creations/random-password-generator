94683345ece752caee34f3d3710ebd1c8020fe54
Savandi <savandikodithuwakku@gmail.com>
2024-07-04 02:05:46 +0530

    Modified css styles

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const numbers='0123456789';
 const symbols='!@#$%^&*()*';
 const hideOrshow=document.querySelector('.result');
 
-
 mySubmit.onclick=function(){
+
     const length= document.getElementById('length').value;
     const includeLowercase= document.getElementById('includeLowercase').checked;
     const includeUppercase= document.getElementById('includeUppercase').checked;
@@ -15,9 +15,7 @@ mySubmit.onclick=function(){
 
     error.textContent='';
     display.value='';
-    hideOrshow.style.display = 'none';
-
-    
+    hideOrshow.style.display = 'none'; /*hide the display*/
 
     let allowedChars='';
     let password='';
@@ -27,19 +25,16 @@ mySubmit.onclick=function(){
     allowedChars += includeSymbols ? symbols: '';
 
     if (length<=0 || length>128 ) {
-        error.textContent='The password length must be between 1 and 128 characters';
-    
+        error.textContent='Password length must be between 1 and 128 characters';
     }
     else if(allowedChars==''){
         error.textContent='At least 1 set of characters need to be included';
-   
     }
     else{
         for(let i=0; i<length; i++){
         const randomIndex= Math.floor(Math.random()*allowedChars.length);
         password+=allowedChars[randomIndex];
-        }
-    
+        }   
     display.value=password;
     hideOrshow.style.display = 'flex'; /*show the display*/
     }
@@ -49,5 +44,3 @@ copy.onclick=function(){
     display.select();
     document.execCommand('copy');
 }
-
-   
